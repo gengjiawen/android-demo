a05ffbc8b1fc9967a0578fb4e8d812ede11b72ce
gengjiawen <technicalcute@gmail.com>
2017-07-05 12:00:59 +0800

    first commit

---
+++ app/src/main/java/gengjiawen/me/switchdemo/MainActivity.kt
@@ -0,0 +1,19 @@
+package gengjiawen.me.switchdemo
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import timber.log.Timber
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Timber.plant(Timber.DebugTree());
+        choose.checkedChanged()
+                .subscribe({
+                    Timber.i("it = " + it)
+                })
+    }
+}
