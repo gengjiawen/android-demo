9b43c27333ff63b4ac42c94c14b663206e0644c1
gengjiawen <technicalcute@gmail.com>
2017-09-26 16:53:07 +0800

    clean up

--- app/src/main/java/me/gengjiawen/switchdemo/MainActivity.kt
+++ app/src/main/java/me/gengjiawen/switchdemo/MainActivity.kt
@@ -2,7 +2,6 @@ package me.gengjiawen.switchdemo
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import timber.log.Timber
 
@@ -13,9 +12,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         Timber.plant(Timber.DebugTree());
 
-        hiButton.setOnClickListener {
-                hiButton.text = "xixi"
-                toast("hi")
-        }
+        hiButton.setOnClickListener({
+            hiButton.text = "xixi"
+            toast("hi")
+        })
     }
 }
