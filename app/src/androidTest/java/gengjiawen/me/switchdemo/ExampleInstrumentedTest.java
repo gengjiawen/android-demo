4288644cc5af5291a9d78c5eb9e2be5e19484fd0
gengjiawen <technicalcute@gmail.com>
2017-11-24 17:54:25 +0800

    daily maintain

--- app/src/androidTest/java/gengjiawen/me/switchdemo/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/gengjiawen/me/switchdemo/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("gengjiawen.me.switchdemo", appContext.getPackageName());
+        assertEquals("me.gengjiawen.quickstart", appContext.getPackageName());
     }
 }
