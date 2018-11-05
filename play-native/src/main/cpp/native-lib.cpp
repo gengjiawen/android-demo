5b5f82239e4081870f58c67032d78869deebe12e
gengjiawen <technicalcute@gmail.com>
2018-11-05 20:37:45 +0800

    maintain and add ndk log

--- play-native/src/main/cpp/native-lib.cpp
+++ play-native/src/main/cpp/native-lib.cpp
@@ -1,5 +1,8 @@
 #include <jni.h>
 #include <string>
+#include <android/log.h>
+
+#define LOGI(...) ((void)__android_log_print(ANDROID_LOG_INFO, "native-lib", __VA_ARGS__))
 
 extern "C" JNIEXPORT jstring
 
@@ -8,5 +11,7 @@ Java_com_gengjiawen_play_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
     std::string hello = "Hello from C++, 耿加稳";
+    int a = 1;
+    LOGI("int %d, string: %s", a, hello.c_str());
     return env->NewStringUTF(hello.c_str());
 }
