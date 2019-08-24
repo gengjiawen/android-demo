95442e011b8a58fba722e85885e702c4c1f39a0c
gengjiawen <technicalcute@gmail.com>
2019-08-24 22:58:20 +0800

    fix import for native module

--- play-native/src/main/java/com/gengjiawen/play/MainActivity.kt
+++ play-native/src/main/java/com/gengjiawen/play/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.gengjiawen.play
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.gengjiawen.play.R
 import kotlinx.android.synthetic.main.activity_main.*
