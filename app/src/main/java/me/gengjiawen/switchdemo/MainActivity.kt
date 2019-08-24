a77ef77bb96edc3de8c92dd89bb38d0ada62d87b
gengjiawen <technicalcute@gmail.com>
2019-08-24 22:45:08 +0800

    Upgrade to agp 3.5.0 and deps update

--- app/src/main/java/me/gengjiawen/switchdemo/MainActivity.kt
+++ app/src/main/java/me/gengjiawen/switchdemo/MainActivity.kt
@@ -1,7 +1,7 @@
 package me.gengjiawen.switchdemo
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import timber.log.Timber
 
